8d44e74c4c790bff17af1c34c58df3fbf971b9ca
Anthony DeGarimore <anthony@pasoroblesconcierge.com>
2026-02-24 16:02:19 -0800

    Improved error handeling

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import wifi
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+wifi = wifi.Wifi()
 
 @app.get("/", status_code=418)
 async def root():
@@ -20,9 +22,13 @@ async def root():
 def status():
     return wifi.status()
 
-@app.get("/wifi/scan")
-def scan():
-    return wifi.scan()
+@app.get("/wifi/networks")
+def networks():
+    return [{
+        "ssid": net.ssid,
+        "signal": net.signal,
+        "security": net.security,
+    } for net in wifi.get_networks()]
 
 @app.post("/wifi/connect")
 def connect(req: dict):
@@ -32,6 +38,7 @@ def connect(req: dict):
     if not ssid:
         raise HTTPException(400, "SSID required")
 
-    if not wifi.connect(ssid, password):
-        return HTTPException(status_code=502, detail="Connection Failed")
-    return Response(status_code=200)
+    success, err = wifi.connect(ssid, password)
+    if not success:
+        raise HTTPException(status_code=502, detail=err)
+    return {"status": "connected"}
--- wifi.py
+++ wifi.py
@@ -1,84 +1,156 @@
+from dataclasses import dataclass
+from datetime import datetime, timedelta
 import subprocess
 import time
 
 
-def run(cmd):
-    return subprocess.check_output(cmd, shell=True, text=True).strip()
+@dataclass
+class Network:
+    ssid: str
+    signal: int
+    security: str
 
 
-def get_wifi_iface():
-    """
-    Return the first Wi-Fi interface detected by NetworkManager.
-    """
-    out = run("nmcli -t -f DEVICE,TYPE dev status")
-    for line in out.splitlines():
-        dev, dev_type = line.split(":")
-        if dev_type == "wifi":
-            return dev
-    raise RuntimeError("No Wi-Fi interfaces found")
+class Wifi:
+
+    def __init__(self):
+        self.wifi_iface: str
+        self.networks: list[Network] = []
+        self.last_scan: datetime = datetime.now()
+
+        out = subprocess.run(["nmcli", "-t", "-f", "DEVICE,TYPE", "dev", "status"], capture_output=True, text=True)
+        for line in out.stdout.splitlines():
+            dev, dev_type = line.split(":")
+            if dev_type == "wifi":
+                self.wifi_iface = dev
+                break
+        if not self.wifi_iface:
+            raise RuntimeError("No Wi-Fi interfaces found")
+
+        self.scan()
 
 
-def status() -> dict:
-    out = run(f"nmcli -t -f TYPE,STATE,CONNECTION dev status")
-    for line in out.splitlines():
-        dev_type, state, conn = line.split(":")
-        if dev_type == "wifi":
-            return {
-                "connected": state == "connected",
-                "ssid": conn if conn != "--" else None
-            }
-    return {
-        "connected": False,
-        "ssid": None
-    }
+
+    @staticmethod
+    def status() -> dict:
+        args = [
+            "nmcli",
+            "-t",
+            "-f",
+            "TYPE,STATE,CONNECTION",
+            "dev",
+            "status"
+        ]
+        out = subprocess.run(args, capture_output=True, text=True)
+        for line in out.stdout.splitlines():
+            dev_type, state, conn = line.split(":")
+            if dev_type == "wifi":
+                return {
+                    "connected": state == "connected",
+                    "ssid": conn if conn != "--" else None
+                }
+        return {
+            "connected": False,
+            "ssid": None
+        }
 
 
-def scan(iface:str=None) -> list[dict]:
-    if not iface:
-        iface = get_wifi_iface()
-    run(f"sudo nmcli dev wifi rescan ifname {iface}")
+    def scan(self) -> None:
+        args = [
+            "sudo",
+            "nmcli",
+            "dev",
+            "wifi",
+            "rescan",
+            "ifname",
+            self.wifi_iface
+        ]
+        subprocess.run(args)
 
-    time.sleep(1.5)
+        time.sleep(2)
 
-    raw = run(
-        f"nmcli -g SSID,SIGNAL,SECURITY dev wifi list ifname {iface} --rescan yes")
+        args = [
+            "nmcli",
+            "-g",
+            "SSID,SIGNAL,SECURITY",
+            "dev",
+            "wifi",
+            "list",
+            "ifname",
+            self.wifi_iface,
+            "--rescan",
+            "yes"
+        ]
+        out = subprocess.run(args, capture_output=True, text=True)
 
-    nets = []
-    seen_ssids = set()
+        self.last_scan = datetime.now()
+        self.networks = []
 
-    for line in raw.splitlines():
-        if not line.strip():
-            continue
+        for line in out.stdout.splitlines():
+            if not line.strip():
+                continue
 
-        ssid, signal, security = line.split(":", 2)
+            ssid, signal, security = line.split(":", 2)
 
-        # Skip blank SSIDs
-        if not ssid or ssid == "--":
-            continue
-
-        # Skip duplicates using a set
-        if ssid in seen_ssids:
-            continue
-
-        seen_ssids.add(ssid)
-        nets.append({
-            "ssid": ssid,
-            "signal": int(signal),
-            "security": security,
-        })
-    return nets
+            self.networks.append(Network(
+                ssid=ssid,
+                signal=int(signal),
+                security=security
+            ))
 
 
-def connect(ssid, password=None) -> bool:
-    """
-    Connect to a Wi-Fi network. If psk is None, assume open network.
-    """
-    scan()
-    try:
+    def get_networks(self) -> list[Network]:
+        if self.last_scan > datetime.now() + timedelta(minutes=1):
+            self.scan()
+
+        filtered_networks = []
+        seen_ssids = set()
+
+        for net in self.networks:
+            # Skip blank SSIDs
+            if not net.ssid or net.ssid == "--":
+                continue
+
+            # Skip duplicates using a set
+            if net.ssid in seen_ssids:
+                continue
+
+            seen_ssids.add(net.ssid)
+            filtered_networks.append(net)
+        return filtered_networks
+
+    @staticmethod
+    def connect(ssid, password=None) -> tuple[bool, str]:
+        """
+        Connect to a Wi-Fi network. If psk is None, assume open network.
+        """
+        args = [
+            "sudo",
+            "nmcli",
+            "dev",
+            "wifi",
+            "connect",
+            ssid
+        ]
         if password:
-            run(f'sudo nmcli dev wifi connect "{ssid}" password "{password}"')
-        else:
-            run(f'sudo nmcli dev wifi connect "{ssid}"')
-        return True
-    except subprocess.CalledProcessError as e:
-        return False
+            args += ["password", password]
+        out = subprocess.run(args, capture_output=True, text=True)
+
+        if out.returncode == 0:
+            return True, ""
+
+        error = (out.stderr or out.stdout).strip().lower()
+        if "no network with ssid" in error:
+            return False, "Network does not exist"
+        elif "secrets were required" in error:
+            if password:
+                return False, "Incorrect password"
+            return False, "Password required"
+        elif "incorrect password" in error or "wrong password" in error:
+            return False, "Incorrect password"
+        elif "property is invalid" in error:
+            return False, "Invalid password"
+        elif "activation failed" in error:
+            return False, "Connection activation failed"
+        return False, f"Unknown error: {error}"
+
